608873e4d638d2c5f8044a56a86c3ee20f1af66e
Vincent Grato <vgrato@gmail.com>
2015-11-13 12:44:52 -0800

    Basic Hike#show with view methods

--- web/views/hike_view.ex
+++ web/views/hike_view.ex
@@ -1,3 +1,13 @@
 defmodule OvernightBackpacker.HikeView do
   use OvernightBackpacker.Web, :view
+
+  @spec elevation(Hike) :: string
+  def elevation(hike) do
+    "+#{hike.elevation_gain} / -#{hike.elevation_loss}"
+  end
+
+  @spec elevation(Hike) :: string
+  def trailhead_lat_long(hike) do
+    "(#{hike.trailhead_lat}, #{hike.trailhead_long})"
+  end
 end
